838e62c3c2475ef30b58bcd3cf03eef0c9dda0aa
Andrey <andrey@gmail.com>
2025-02-20 21:48:06 +0200

    Сделал логин пейдж, так же добавил возможность войти на страницу, завтра буду заниматься оформлением главной страницы

---
+++ js/login.js
@@ -0,0 +1,43 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	class User {
+		constructor(login, password) {
+			this.login = login;
+			this.password = password;
+		}
+	}
+
+	const admin = new User('admin', '12345678');
+
+	const login = document.querySelector('input[name="login"]'),
+		  password = document.querySelector('input[name="password"]'),
+		  loginBtn = document.querySelector('button'),
+		  wrapper = document.querySelector('.wrapper');
+	
+	loginBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		if (login.value === admin.login && password.value === admin.password) {
+			window.location.href = 'biblio.html';
+		} else {
+			const div = document.createElement('div');
+			div.classList.add('error');
+
+			if (login.value.trim() == '' || password.value.trim() == '') {
+				div.textContent = 'Заполните все поля!';
+				setTimeout(() => {
+					div.remove();
+				}, 3000);
+			} else {
+				div.textContent = 'У вас недостаточно прав, возможность регистрации будет предоставлена в ближайшем будущем.';
+				setTimeout(() => {
+					div.remove();
+				}, 3000)
+			}
+
+			wrapper.appendChild(div);
+		}
+	});
+});
